4bb26a2b79b8c17733195f9380fd684f09b32d46
Harry Gordon <Henry.Gordon@capco.com>
2016-06-24 17:35:14 +0100

    half way through login with json

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,12 @@
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
 
+var bcrypt   = require('bcrypt-nodejs');
+
 // load up the user model
 var User            = require('../app/models/user');
+var fs = require("fs");
+var myJson;
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
@@ -15,6 +19,7 @@ module.exports = function(passport) {
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
+        console.log(user);
         done(null, user.id);
     });
 
@@ -38,42 +43,52 @@ module.exports = function(passport) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) {
+        var newUser            = new User();
 
-        // asynchronous
-        // User.findOne wont fire unless data is sent back
-        process.nextTick(function() {
+        myJson = {
+            id: '576d3cbc7fb2714b68884f99',
+            email: email,
+            password: bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+        };
 
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error
-            if (err)
-                return done(err);
+        fs.writeFile( "local.json", JSON.stringify( myJson ), "utf8");
 
-            // check to see if theres already a user with that email
-            if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } else {
+        done(null, myJson);
 
-                // if there is no user with that email
-                // create the user
-                var newUser            = new User();
-
-                // set the user's local credentials
-                newUser.local.email    = email;
-                newUser.local.password = newUser.generateHash(password);
-
-                // save the user
-                newUser.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, newUser);
-                });
-            }
-
-        });
-
-        });
+        // // asynchronous
+        // // User.findOne wont fire unless data is sent back
+        // process.nextTick(function() {
+        //
+        // // find a user whose email is the same as the forms email
+        // // we are checking to see if the user trying to login already exists
+        // User.findOne({ 'local.email' :  email }, function(err, user) {
+        //     // if there are any errors, return the error
+        //     if (err)
+        //         return done(err);
+        //
+        //     // check to see if theres already a user with that email
+        //     if (user) {
+        //         return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        //     } else {
+        //
+        //         // if there is no user with that email
+        //         // create the user
+        //
+        //         // set the user's local credentials
+        //         newUser.local.email    = email;
+        //         newUser.local.password = newUser.generateHash(password);
+        //         console.log(newUser);
+        //         // save the user
+        //         newUser.save(function(err) {
+        //             if (err)
+        //                 throw err;
+        //             return done(null, newUser);
+        //         });
+        //     }
+        //
+        // });
+        //
+        // });
 
     }));
 
@@ -90,25 +105,36 @@ module.exports = function(passport) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) { // callback with email and password from our form
+        myJson = require("local.json");
 
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error before anything else
-            if (err)
-                return done(err);
+        if(email === myJson.email){
+            if(bcrypt.compareSync(password, myJson.password)) {
+                console.log('success')
+                return done(null, myJson);
+            }
+        }
 
-            // if no user is found, return the message
-            if (!user)
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+        done();
 
-            // if the user is found but the password is wrong
-            if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-            // all is well, return successful user
-            return done(null, user);
-        });
+        // // find a user whose email is the same as the forms email
+        // // we are checking to see if the user trying to login already exists
+        // User.findOne({ 'local.email' :  email }, function(err, user) {
+        //     // if there are any errors, return the error before anything else
+        //     if (err)
+        //         return done(err);
+        //
+        //     // if no user is found, return the message
+        //     if (!user)
+        //         return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+        //
+        //     // if the user is found but the password is wrong
+        //     if (!user.validPassword(password))
+        //         return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+        //
+        //     // all is well, return successful user
+        //     return done(null, user);
+        // });
 
     }));
 
